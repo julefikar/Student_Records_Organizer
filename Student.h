3639ffe1b492ed8e1ac21b712f60a4fb1edde5b8
julefikar <julfikar.chowdhury50@myhunter.cuny.edu>
2022-07-04 14:52:31 -0400

    Student info header file

---
+++ Student.h
@@ -0,0 +1,16 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+
+class Student
+{
+public:
+	unsigned int studentId;
+	std::string name;
+	unsigned short age;
+	//Student(unsigned int, std::string, unsigned short);
+};
+
+
+#endif // !STUDENT_H
